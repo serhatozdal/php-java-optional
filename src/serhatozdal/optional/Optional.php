18d7a95d6b85e00ffe8795432eeae79ca99bba90
serhatozdal <serhatozdal@gmail.com>
2018-04-12 23:19:39 +0300

    first version of java8 php optional

---
+++ src/serhatozdal/optional/Optional.php
@@ -0,0 +1,99 @@
+<?php
+namespace serhatozdal\optional;
+/**
+ * @author serhatozdal
+ */
+final class Optional
+{
+    private $value;
+
+    private function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public static function ofEmpty()
+    {
+        return new self(null);
+    }
+
+    public static function of($value)
+    {
+        return new self(self::requireNonNull($value));
+    }
+
+    public static function ofNullable($value)
+    {
+        return $value === null ? self::ofEmpty() : self::of($value);
+    }
+
+    public function get()
+    {
+        return self::requireNonNull($this->value);
+    }
+
+    public function isPresent()
+    {
+        return $this->value !== null;
+    }
+
+    public function ifPresent(callable $consumer)
+    {
+        if ($this->isPresent())
+            $consumer($this->value);
+    }
+
+    public function filter(callable $predicate)
+    {
+        self::ofNullable($predicate);
+        if (!$this->isPresent())
+            return $this;
+        else
+            return $predicate($this->value) ? $this : self::ofEmpty();
+    }
+
+    public function map(callable $mapper)
+    {
+        self::requireNonNull($mapper);
+        if (!$this->isPresent())
+            return self::ofEmpty();
+        else
+            return self::ofNullable($mapper($this->value));
+    }
+
+    public function flatMap(callable $mapper)
+    {
+        self::requireNonNull($mapper);
+        if (!$this->isPresent())
+            return self::ofEmpty();
+        else
+            return self::requireNonNull($mapper($this->value));
+    }
+
+    public function orElse($other)
+    {
+        return $this->isPresent() ? $this->value : $other;
+    }
+
+    public function orElseGet(callable $other)
+    {
+        return $this->isPresent() ? $this->value : $other();
+    }
+
+    public function orElseThrow(callable $exceptionSupplier)
+    {
+        return $this->isPresent() ? $this->value : $exceptionSupplier();
+    }
+
+    private static function requireNonNull($obj)
+    {
+        if ($obj === null)
+            throw new InvalidArgumentException("variable can not be null!!");
+        return $obj;
+    }
+
+    public function __toString()
+    {
+        return $this->isPresent() ? sprintf("Optional[%s]", $this->value) : "Optional.ofEmpty";
+    }
+}
